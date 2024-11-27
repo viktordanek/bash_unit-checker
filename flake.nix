206cf897132f38840f54815e106c34511b1b0467
Emory Merryman IS 300 <viktordanek10@gmail.com>
2024-11-27 14:07:16 -0500

    yes

--- flake.nix
+++ flake.nix
@@ -96,13 +96,19 @@
                                                 lib
                                                     {
                                                         expected-path = ./expected ;
-                                                        observed = "774cee76b63f8da4a28aac5aa644d7bb3cb5ff12274e43060136cdad41a353ab3b25281124900b9e07c513e815373b6fe025ee647ede2225825de9f6c216f555" ;
+                                                        observed =
+                                                            ''
+                                                                ${ pkgs.coreutils }/bin/echo 774cee76b63f8da4a28aac5aa644d7bb3cb5ff12274e43060136cdad41a353ab3b25281124900b9e07c513e815373b6fe025ee647ede2225825de9f6c216f555 > ${ environment-variable "OBSERVED" }
+                                                            '' ;
                                                     } ;
                                             success =
                                                 lib
                                                     {
                                                         expected-path = ./expected ;
-                                                        observed = "bb882270c0d417368b5d4b08bbdfb27c772137e5b79265422d8d0245ce923f336f4ce661b8b341de1fb2f82fe5b249dbc409b98c45ab6082baf0e983000e93f9" ;
+                                                        observed =
+                                                            ''
+                                                                ${ pkgs.coreutils }/bin/echo bb882270c0d417368b5d4b08bbdfb27c772137e5b79265422d8d0245ce923f336f4ce661b8b341de1fb2f82fe5b249dbc409b98c45ab6082baf0e983000e93f9 > ${ environment-variable "OBSERVED" }
+                                                            '' ;
                                                     } ;
                                                 test =
                                                     derivation : status :
